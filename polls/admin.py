23d7f9836536907b3f8268f33167289da098e511
hfc <jon@highfivecode.com>
2018-02-19 16:18:55 -0500

    CRUD with the admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Poll
+
 # Register your models here.
+admin.site.register(Poll)
--- polls/models.py
+++ polls/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Poll(models.Model):
     text = models.CharField(max_length=255)
     pub_date = models.DateField()
+
+    def __str__(self):
+        return self.text
