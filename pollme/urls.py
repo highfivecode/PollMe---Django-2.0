a544819253611d9cc2d2249a5ba70595a296747f
hfc <jon@highfivecode.com>
2018-02-19 18:14:17 -0500

    including the polls apps;

--- pollme/urls.py
+++ pollme/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('time/', views.current_datetime),
-    path('home/', views.home)
+    path('home/', views.home),
+    path('polls/', include('polls.urls'))
 ]
