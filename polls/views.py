5316fc7ebec45f07dd28404271910851875af314
hfc <jon@highfivecode.com>
2018-02-23 17:55:54 -0500

    using querysets in the templates

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Poll
+
 # Create your views here.
 def polls_list(request):
-    return render(request, 'polls/polls_list.html')
+    """
+    Renders the polls_list.html template which lists all the
+    currently available polls
+    """
+    polls = Poll.objects.all()
+    context = {'polls': polls}
+    return render(request, 'polls/polls_list.html', context)
