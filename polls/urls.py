f16933da4575af5f15a361bef1e01a5a0b2c2f46
hfc <jon@highfivecode.com>
2018-02-24 08:57:32 -0500

    how to capture values from the url

--- polls/urls.py
+++ polls/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 app_name="polls"
 urlpatterns = [
-    path('list/', views.polls_list, name="list")
+    path('list/', views.polls_list, name="list"),
+    # polls/details/1/
+    path('details/<int:poll_id>/', views.poll_detail, name='detail')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Poll
 
@@ -11,3 +12,6 @@ def polls_list(request):
     polls = Poll.objects.all()
     context = {'polls': polls}
     return render(request, 'polls/polls_list.html', context)
+
+def poll_detail(request, poll_id):
+    return HttpResponse('Youre looking for poll with and id off: {}'.format(poll_id))
