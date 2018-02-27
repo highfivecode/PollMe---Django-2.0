5f867676dd09a7b56e3513b9bb29903c8b88f858
hfc <jon@highfivecode.com>
2018-02-26 19:02:44 -0500

    the login view

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name="accounts"
 urlpatterns = [
-    path('login/', views.login, name="login"),
+    path('login/', views.login_user, name="login"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,20 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 
 # Create your views here.
-def login(request):
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            messages.error(request, 'Bad username or password')
+
     return render(request, 'accounts/login.html', {})
