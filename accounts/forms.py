347245f5b59bbab15aa392f2b50e7947ff7d135b
hfc <jon@highfivecode.com>
2018-03-16 12:27:02 -0400

    48 Styling The Registration Form

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,10 +3,22 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100, min_length=5)
-    email = forms.EmailField()
-    password1 = forms.CharField(label="Password", max_length=100, min_length=5)
-    password2 = forms.CharField(label="Confirm Password", max_length=100, min_length=5)
+    username = forms.CharField(
+                        label='Username',
+                        max_length=100,
+                        min_length=5,
+                        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(
+                            label="Password",
+                            max_length=100,
+                            min_length=5,
+                            widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(
+                            label="Confirm Password",
+                            max_length=100,
+                            min_length=5,
+                            widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
     def clean_email(self):
         email = self.cleaned_data['email']
