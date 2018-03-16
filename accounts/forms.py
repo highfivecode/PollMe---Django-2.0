4ec82202229248dde28eeac138c6890baa0ae3ad
hfc <jon@highfivecode.com>
2018-03-16 12:03:14 -0400

    47 - Password Field Validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,3 +14,28 @@ class UserRegistrationForm(forms.Form):
         if qs.exists():
             raise ValidationError('Email is already registered.')
         return email
+
+    # def clean_password1(self):
+    #     # this will raise a key error because it references password2 field
+    #     # which is ran AFTER password1 validation
+    #     p1 = self.cleaned_data['password1']
+    #     p2 = self.cleaned_data['password2']
+    #     if p1 != p2:
+    #         raise ValidationError('Password1 Does Not Match Password2')
+    #     return p1
+
+    # def clean_password2(self):
+    #     p1 = self.cleaned_data['password1']
+    #     p2 = self.cleaned_data['password2']
+    #     if p1 != p2:
+    #         raise ValidationError('Password2 Does Not Match Password1')
+    #     return p2
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('password2')
+
+        if p1 and p2:
+            if p1 != p2:
+                raise ValidationError('Passwords Do Not Match')
