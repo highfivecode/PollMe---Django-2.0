fff2cb16c0382be2c4dd3306c51e60afb3eeadbe
hfc <jon@highfivecode.com>
2018-03-16 09:13:01 -0400

    41 User Registration Path View Template

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name="accounts"
 urlpatterns = [
     path('login/', views.login_user, name="login"),
-    path('logout/', views.logout_user, name='logout')
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.user_registration, name="register")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from .forms import UserRegistrationForm
+
 
 # Create your views here.
 def login_user(request):
@@ -22,3 +24,7 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
+
+def user_registration(request):
+    form = UserRegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
