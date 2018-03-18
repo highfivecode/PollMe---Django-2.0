80beb0edc1fe1269c4d5602849fa6516ccdb9436
hfc <jon@highfivecode.com>
2018-03-17 22:07:30 -0400

    60 - Edit Poll Page and Allowing Only The Owner

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ app_name="polls"
 urlpatterns = [
     path('list/', views.polls_list, name="list"),
     path('add/', views.add_poll, name='add'),
+    path('edit/<int:poll_id>/', views.edit_poll, name='edit_poll'),
     # polls/details/1/
     path('details/<int:poll_id>/', views.poll_detail, name='detail'),
     # polls/details/1/vote
--- polls/views.py
+++ polls/views.py
@@ -51,6 +51,13 @@ def add_poll(request):
     return render(request, 'polls/add_poll.html', context)
 
 @login_required
+def edit_poll(request, poll_id):
+    poll = get_object_or_404(Poll, id=poll_id)
+    if request.user != poll.owner:
+        return redirect('/')
+    return render(request, 'polls/edit_poll.html', {'poll':poll})
+
+@login_required
 def poll_detail(request, poll_id):
     """
     Render the poll_detail.html template which allows a user to vote
